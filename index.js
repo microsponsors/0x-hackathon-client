a0eb41f2ca033c7e9f4fb042a1979b75bae77127
Lauren Garcia <lauren.garcia@gmail.com>
2019-01-14 20:25:35 -0800

    client debug stuff

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 'use strict';
+// Debug client on CLI
 Object.defineProperty(exports, '__esModule', { value: true });
 const Connect = require('@0x/connect');
 const client = new Connect.HttpClient('http://localhost:3000/v2');
 
+console.log(`\n\n\n-- Client Configuration:`)
 client.getFeeRecipientsAsync()
   .then(
-    (r) => console.log(r),
-    (e) => console.log(e)
+    (r) => {
+      console.log('Fee Receipients: ')
+      console.log(r)
+    },
+    (e) => console.error(e)
+  )
+
+client.getAssetPairsAsync()
+  .then(
+    (r) => {
+      console.log('Asset Pairs: ')
+      console.log(r)
+      if (r.records) {
+        r.records.forEach((record) => { console.log(record) })
+      }
+    },
+    (e) => console.error(e)
+  )
+
+client.getOrdersAsync()
+  .then(
+    (r) => {
+      console.log('\n\nCurrent Orders: ')
+      console.log(r)
+      if (r.records) {
+        r.records.forEach((record) => {
+          console.log('Order:')
+          console.log(record)
+        })
+      }
+    },
+    (e) => console.error(e)
   )
 
